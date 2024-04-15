621bb3d257ece7e106b1b9c02199ce5f4d5adbe5
QWolfPlaY <st30943@gmail.com>
2024-04-15 18:32:48 +0200

    Create Leggings class

---
+++ src/Armor.abstract/Leggings.class/Leggings.cpp
@@ -0,0 +1,22 @@
+//
+// Created by WolfPlay on 4/15/2024.
+//
+
+#include "Leggings.h"
+
+Leggings::Leggings(std::string name, std::string description, float price, Item::type type, Item::rarity rarity,
+                   float baseDefence, float baseResistance)
+        : Armor(name, description, price, type, rarity, baseDefence, baseResistance)
+{}
+
+Leggings::~Leggings() = default;
+
+float Leggings::calculateMeleeDamageReduction(float damage) const
+{
+    return (damage - (damage * (getBaseDefence() / 100.0))) * 0.45;
+}
+
+float Leggings::calculateMagicDamageReduction(float damage) const
+{
+    return (damage - (damage * (getBaseResistance() / 100.0))) * 0.49;
+}
