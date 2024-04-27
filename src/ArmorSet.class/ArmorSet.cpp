602a6721d5f74cdcb487be7e0e81019dcd73b607
QWolfPlaY <st30943@gmail.com>
2024-04-27 21:22:22 +0200

    Create ArmorSet class instead of dumb ArmorSet struct made in Player class

---
+++ src/ArmorSet.class/ArmorSet.cpp
@@ -0,0 +1,123 @@
+//
+// Created by WolfPlay on 4/27/2024.
+//
+
+#include "ArmorSet.h"
+
+#include <stdexcept>
+
+#include "Exceptions/SlotEmptyException.h"
+#include "Exceptions/SlotAlreadyOccupiedException.h"
+
+ArmorSet::ArmorSet()
+{
+    _helmet = nullptr;
+    _chestplate = nullptr;
+    _leggings = nullptr;
+
+    _isHelmetEquipped = false;
+    _isChestplateEquipped = false;
+    _isLeggingsEquipped = false;
+}
+
+ArmorSet::ArmorSet(Helmet *helmet, Chestplate *chestplate, Leggings *leggings)
+{
+    _helmet = helmet;
+    _chestplate = chestplate;
+    _leggings = leggings;
+
+    _isHelmetEquipped = helmet != nullptr;
+    _isChestplateEquipped = chestplate != nullptr;
+    _isLeggingsEquipped = leggings != nullptr;
+}
+
+ArmorSet::~ArmorSet()
+{
+    delete _helmet;
+    delete _chestplate;
+    delete _leggings;
+}
+
+Helmet *ArmorSet::getHelmetPtr() const
+{
+    if (!_isHelmetEquipped) { throw SlotEmptyException(); }
+    return _helmet;
+}
+
+Chestplate *ArmorSet::getChestplatePtr() const
+{
+    if (!_isChestplateEquipped) { throw SlotEmptyException(); }
+    return _chestplate;
+}
+
+Leggings *ArmorSet::getLeggingsPtr() const
+{
+    if (!_isLeggingsEquipped) { throw SlotEmptyException(); }
+    return _leggings;
+}
+
+bool ArmorSet::isHelmetEquipped() const
+{
+    return _isHelmetEquipped;
+}
+
+bool ArmorSet::isChestplateEquipped() const
+{
+    return _isChestplateEquipped;
+}
+
+bool ArmorSet::isLeggingsEquipped() const
+{
+    return _isLeggingsEquipped;
+}
+
+void ArmorSet::equipHelmet(Helmet *helmet)
+{
+    if (_isHelmetEquipped) { throw SlotAlreadyOccupiedException(); }
+    if (helmet == nullptr) { throw std::invalid_argument("ArmorSet::equipHelmet: helmet is nullptr"); }
+    _helmet = helmet;
+    _isHelmetEquipped = true;
+}
+
+void ArmorSet::equipChestplate(Chestplate *chestplate)
+{
+    if (_isChestplateEquipped) { throw SlotAlreadyOccupiedException(); }
+    if (chestplate == nullptr) { throw std::invalid_argument("ArmorSet::equipChestplate: chestplate is nullptr"); }
+    _chestplate = chestplate;
+    _isChestplateEquipped = true;
+}
+
+void ArmorSet::equipLeggings(Leggings *leggings)
+{
+    if (_isLeggingsEquipped) { throw SlotAlreadyOccupiedException(); }
+    if (leggings == nullptr) { throw std::invalid_argument("ArmorSet::equipLeggings: leggings is nullptr"); }
+    _leggings = leggings;
+    _isLeggingsEquipped = true;
+}
+
+Helmet *ArmorSet::unequipHelmet()
+{
+    if (!_isHelmetEquipped) { throw SlotEmptyException(); }
+    Helmet *helmet = _helmet;
+    _helmet = nullptr;
+    _isHelmetEquipped = false;
+    return helmet;
+}
+
+Chestplate *ArmorSet::unequipChestplate()
+{
+    if (!_isChestplateEquipped) { throw SlotEmptyException(); }
+    Chestplate *chestplate = _chestplate;
+    _chestplate = nullptr;
+    _isChestplateEquipped = false;
+    return chestplate;
+}
+
+Leggings *ArmorSet::unequipLeggings()
+{
+    if (!_isLeggingsEquipped) { throw SlotEmptyException(); }
+    Leggings *leggings = _leggings;
+    _leggings = nullptr;
+    _isLeggingsEquipped = false;
+    return leggings;
+}
---
+++ src/ArmorSet.class/ArmorSet.h
@@ -0,0 +1,56 @@
+//
+// Created by WolfPlay on 4/27/2024.
+//
+
+#ifndef RPG_ARMORSET_H
+#define RPG_ARMORSET_H
+
+#include "Armor.abstract/Armor.h"
+#include "Armor.abstract/Helmet.class/Helmet.h"
+#include "Armor.abstract/Chestplate.class/Chestplate.h"
+#include "Armor.abstract/Leggings.class/Leggings.h"
+
+class ArmorSet
+{
+    Helmet *_helmet;
+    Chestplate *_chestplate;
+    Leggings *_leggings;
+
+    bool _isHelmetEquipped;
+    bool _isChestplateEquipped;
+    bool _isLeggingsEquipped;
+
+public:
+    ArmorSet();
+
+    ArmorSet(Helmet *helmet, Chestplate *chestplate, Leggings *leggings);
+
+    ~ArmorSet();
+
+    Helmet *getHelmetPtr() const;
+
+    Chestplate *getChestplatePtr() const;
+
+    Leggings *getLeggingsPtr() const;
+
+    [[nodiscard]] bool isHelmetEquipped() const;
+
+    [[nodiscard]] bool isChestplateEquipped() const;
+
+    [[nodiscard]] bool isLeggingsEquipped() const;
+
+    void equipHelmet(Helmet *helmet);
+
+    void equipChestplate(Chestplate *chestplate);
+
+    void equipLeggings(Leggings *leggings);
+
+    Helmet *unequipHelmet();
+
+    Chestplate *unequipChestplate();
+
+    Leggings *unequipLeggings();
+};
+
+
+#endif //RPG_ARMORSET_H
