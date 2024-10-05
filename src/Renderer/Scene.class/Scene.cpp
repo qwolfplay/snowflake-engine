2b8f4440e4abbcd29c2e2b3af74b631c72fb1a35
WolfPlay <qwolfplay@proton.me>
2024-10-06 00:04:21 +0200

    Create `Scene` and `UIElement` classes

---
+++ src/Renderer/Scene.class/Scene.cpp
@@ -0,0 +1,9 @@
+//
+// Created by WolfPlay on 7/14/2024.
+//
+
+#include "Scene.h"
+
+namespace Snowflake
+{
+} // Snowflake
---
+++ src/Renderer/Scene.class/Scene.h
@@ -0,0 +1,23 @@
+//
+// Created by WolfPlay on 7/14/2024.
+//
+
+#ifndef SNOWFLAKE_SCENE_H
+#define SNOWFLAKE_SCENE_H
+
+namespace Snowflake
+{
+
+    class Scene
+    {
+
+        void update();
+
+    public:
+
+        void render();
+    };
+
+} // Snowflake
+
+#endif //SNOWFLAKE_SCENE_H
