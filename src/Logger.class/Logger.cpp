d5b8868e0d6d7484c92e981bfc95b70d4fbfd5ac
WolfPlay <qwolfplay@proton.me>
2024-10-06 19:26:03 +0200

    Make the logger actually work

--- src/Logger.class/Logger.cpp
+++ src/Logger.class/Logger.cpp
@@ -9,6 +9,7 @@
 
 #include "spdlog/async.h"
 #include "spdlog/fmt/chrono.h"
+#include "spdlog/sinks/ansicolor_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/basic_file_sink.h"
 
@@ -28,9 +29,19 @@ Logger::Logger(): _name("Logger") {
     auto t = std::chrono::system_clock::now();
     std::vector<spdlog::sink_ptr> sinks;
 
-    sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(fmt::format("{:%F-%T.log}", t)));
+    const auto stdoutSinkSharedPtr = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
+    // Idea: Add ability to read level colors from a config file
+    stdoutSinkSharedPtr->set_color(spdlog::level::err, 0xff0000);
+    stdoutSinkSharedPtr->set_pattern(""); //TODO
+
+    sinks.push_back(stdoutSinkSharedPtr);
+
+    // FIXME: This sink causes a (non-critical) 0xe06d7363 exception when called
+    // const auto fileSinkSharedPtr   = std::make_shared<spdlog::sinks::basic_file_sink_mt>(fmt::format("{:%F-%T.log}", t));
+    // sinks.push_back(fileSinkSharedPtr);
+
+    spdlog::init_thread_pool(10240, 2);
     _asyncLogger = std::make_shared<spdlog::async_logger>(
                                                           _name,
                                                           begin(sinks),
